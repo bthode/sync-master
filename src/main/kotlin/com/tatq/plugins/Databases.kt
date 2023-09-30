a93530fd68e131bcc48fa955b4f852a915ab6224
Bryan Thode <bryan@tatq.com>
2023-09-30 06:56:08 -0500

    Ignore swagger codegen file Lift if statement in Databases.kt

--- src/main/kotlin/com/tatq/plugins/Databases.kt
+++ src/main/kotlin/com/tatq/plugins/Databases.kt
@@ -72,13 +72,13 @@ fun Application.configureDatabases() {
  * */
 fun Application.connectToPostgres(embedded: Boolean): Connection {
     Class.forName("org.postgresql.Driver")
-    if (embedded) {
-        return DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
+    return if (embedded) {
+        DriverManager.getConnection("jdbc:h2:mem:test;DB_CLOSE_DELAY=-1", "root", "")
     } else {
         val url = environment.config.property("postgres.url").getString()
         val user = environment.config.property("postgres.user").getString()
         val password = environment.config.property("postgres.password").getString()
 
-        return DriverManager.getConnection(url, user, password)
+        DriverManager.getConnection(url, user, password)
     }
 }
